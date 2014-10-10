2b1a4789ef934ddec2c1e486989dcbea6debfaeb
Dan Vassallo <danielvassallo87@gmail.com>
2014-10-09 22:30:57 -0400

    Fixed Pages

--- config.rb
+++ config.rb
@@ -47,7 +47,7 @@ configure :development do
 #   end
 # end
 
-activate :directory_indexes
+set :index_file, "default.html"
 
 set :css_dir, 'stylesheets'
 
@@ -64,10 +64,10 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-   activate :asset_hash
+  # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
